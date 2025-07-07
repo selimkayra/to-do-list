127d962317cff914f643b4bd521ff7d69c06aa1e
selimkayra <slmkyr58@gmail.com>
2025-07-07 15:06:18 +0300

    To Do List
    HTML, CSS ve JS ile basit to do list örneği.

---
+++ script.js
@@ -0,0 +1,50 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Boş bırakılamaz!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        const onay = confirm("Silmek istediğinden emin misin?");
+        if (onay) {
+        e.target.parentElement.remove();
+        saveData();
+    }
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
+
+inputBox.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        event.preventDefault();
+        addTask();
+    }
+});
